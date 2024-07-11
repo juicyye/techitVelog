f4ceb5a4ad24c8f362073741d282f292aa862c80
param <spsummerdoc@gmail.com>
2024-07-11 10:43:34 +0900

    feat: "comment 유효성 검사 추가"

--- src/main/java/techit/velog/domain/comment/dto/webreq/CommentReqDtoWebUpdate.java
+++ src/main/java/techit/velog/domain/comment/dto/webreq/CommentReqDtoWebUpdate.java
@@ -1,5 +1,7 @@
 package techit.velog.domain.comment.dto.webreq;
 
+import jakarta.validation.constraints.NotEmpty;
+import jakarta.validation.constraints.Size;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
@@ -8,5 +10,7 @@ import lombok.NoArgsConstructor;
 @AllArgsConstructor
 public class CommentReqDtoWebUpdate {
     private Long commentId;
+    @NotEmpty
+    @Size(min = 2, message = "2글자 이상 적어주세요")
     private String content;
 }
