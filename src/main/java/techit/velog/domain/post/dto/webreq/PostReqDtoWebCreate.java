59689d02327d540d97900cf725c8016a050f2cfb
param <spsummerdoc@gmail.com>
2024-07-11 20:46:10 +0900

    fix: "post 공백도 들어갈수 있도록 작성"

--- src/main/java/techit/velog/domain/post/dto/webreq/PostReqDtoWebCreate.java
+++ src/main/java/techit/velog/domain/post/dto/webreq/PostReqDtoWebCreate.java
@@ -12,12 +12,12 @@ import java.util.List;
 @Data
 public class PostReqDtoWebCreate {
     @NotEmpty
-    @Pattern(regexp = "^(?=.*[ㄱ-ㅎ가-힣a-zA-Z0-9])[ㄱ-ㅎ가-힣a-zA-Z0-9\\s]{2,18}[ㄱ-ㅎ가-힣a-zA-Z0-9]$",message = "제목은 2~20 이내로 적어주세요")
+    @Pattern(regexp = "^(?=.*[ㄱ-ㅎ가-힣a-zA-Z0-9])[ㄱ-ㅎ가-힣a-zA-Z0-9\\s]{1,18}[ㄱ-ㅎ가-힣a-zA-Z0-9]$",message = "제목은 2~20 이내로 적어주세요")
     private String title;
     @NotEmpty
     private String content;
     @NotEmpty
-    @Pattern(regexp = "^(?=.*[ㄱ-ㅎ가-힣a-zA-Z0-9])[ㄱ-ㅎ가-힣a-zA-Z0-9\\s]{2,18}[ㄱ-ㅎ가-힣a-zA-Z0-9]$", message = "2~20자 이내로 적어주세요")
+    @Pattern(regexp = "^(?=.*[ㄱ-ㅎ가-힣a-zA-Z0-9])[ㄱ-ㅎ가-힣a-zA-Z0-9\\s]{1,18}[ㄱ-ㅎ가-힣a-zA-Z0-9]$", message = "2~20자 이내로 적어주세요")
     private String postDescription;
     private IsSecret isSecret;
     private Boolean isTemp;
