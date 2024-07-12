f16df029b94183957fcda6c820f08ffb027da953
param <spsummerdoc@gmail.com>
2024-07-12 16:39:23 +0900

    refactor: "email service 엔터"

--- src/main/java/techit/velog/domain/mail/event/MailEventHandler.java
+++ src/main/java/techit/velog/domain/mail/event/MailEventHandler.java
@@ -2,8 +2,6 @@ package techit.velog.domain.mail.event;
 
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.context.event.EventListener;
-import org.springframework.mail.SimpleMailMessage;
 import org.springframework.scheduling.annotation.Async;
 import org.springframework.stereotype.Component;
 import org.springframework.transaction.event.TransactionalEventListener;
